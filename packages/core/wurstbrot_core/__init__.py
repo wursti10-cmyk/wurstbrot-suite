3c72f37e2221f5696851a45ead0a2c80ea361562
wursti10-cmyk <wursti10@googlemail.com>
2026-08-05 13:11:45 +0200

    Prepare 0.9.0 beta: update packages/core/wurstbrot_core/__init__.py

--- packages/core/wurstbrot_core/__init__.py
+++ packages/core/wurstbrot_core/__init__.py
@@ -1,5 +1,7 @@
 """Wurstbrot GE Calculator 2.0 core engine."""
 
+__version__ = "0.9.0-beta"
+
 from .database import VehicleDatabase
 from .models import PlayerProgress, SolveOptions, SolveResult, VehicleProgress
 from .solver import ResearchSolver
@@ -11,4 +13,5 @@ __all__ = [
     "SolveOptions",
     "SolveResult",
     "ResearchSolver",
+    "__version__",
 ]
