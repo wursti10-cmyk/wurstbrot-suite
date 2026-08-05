f7393643a0d18c998f3e04f1bc21a0d7bf13fb24
wursti10-cmyk <wursti10@googlemail.com>
2026-08-05 13:11:59 +0200

    Prepare 0.9.0 beta: add apps/web/service-worker.js

---
+++ apps/web/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE = "wurstbrot-0.9.0-beta";
+const ASSETS = ["./", "index.html", "styles.css", "app.js", "solver.mjs", "manifest.webmanifest", "icon.svg", "../../data/samples/WT_Database_2.57.1.67.json"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
